79ba75a3b5c0aa2b063ac8ed62cc4bf002bcd4ee
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-18 01:20:08 +0100

    Terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 
 class Agenda(contacto: String = "", numero: String = ""){
 
-    val listaContactos = mutableListOf<String>()
-    val listaNumeros = mutableListOf<String>()
+    val listaContactos = mutableListOf<String>("Juan","Pedro","Helena")
+    val listaNumeros = mutableListOf<String>("+34634581940","+34947102064","+34634539940")
 
     fun añadirContacto(contacto:String){
         //Esta variable se pondrá en true si encuentra en la lista el contacto introducido
@@ -11,18 +11,37 @@ class Agenda(contacto: String = "", numero: String = ""){
         var posicionIgual: Int = 0
         listaContactos.forEach {if (it==contacto) {igualEncontrado = true; posicionIgual = listaContactos.indexOf(it)}}
         //Creo una variable Introducido para usarlo abajo. Esta variable cuando es true indicará que se ha metido en el primer vacio o null de la lista
-        var Introducido: Boolean = false
-        if (igualEncontrado == true) { println("El contacto ya existe y este es su número ${listaNumeros[posicionIgual]}")}
-        else {println("Introduce un número para registrarlo con el contacto: "); var numero = readLine().toString(); while (Introducido != true) {var i = 0;if (listaContactos[i].isNullOrEmpty()) {listaNumeros[i] = numero; listaContactos[i] = contacto} else {i++}} }}
+        var Introducido: Boolean = false; var i = 0; var numero = ""
+        //Voy a introducirlo buscando si hay un null, como en la lista no hay nulls soltará un IndexoutOfBoundsException así que le hago un try catch para simplemente añadirlo con un .add en ese caso
+        try{if (igualEncontrado == true) { println("El contacto ya existe y este es su número: ${listaNumeros[posicionIgual]}")}
+        else {println("Introduce un número para registrarlo con el contacto: "); numero = readLine().toString();while (Introducido != true) {if (listaContactos[i].isNullOrEmpty()) {listaNumeros[i] = numero; listaContactos[i] = contacto; Introducido = true} else {i++}} }}
+        catch (e: IndexOutOfBoundsException) {listaContactos.add(contacto); listaNumeros.add(numero)}
+    }
 
     fun añadirNumero(numero: String){
         //El funcionamiento es igual que en añadirContacto pero intercambiando Numero y Contacto
         var igualEncontrado: Boolean = false
         var posicionIgual: Int = 0
-        listaContactos.forEach {if (it==numero) {igualEncontrado = true; posicionIgual = listaNumeros.indexOf(it)}}
-        var Introducido: Boolean = false
-        if (igualEncontrado == true) { println("El número ya existe y este es su contacto ${listaContactos[posicionIgual]}")}
-        else {println("Introduce un número para registrarlo con el contacto: "); var contacto = readLine().toString(); while (Introducido != true) {var i = 0;if (listaNumeros[i].isNullOrEmpty()) {listaNumeros[i] = numero; listaContactos[i] = contacto} else {i++}} }
+        listaNumeros.forEach {if (it==numero) {igualEncontrado = true; posicionIgual = listaNumeros.indexOf(it)}}
+        var Introducido: Boolean = false; var i = 0; var contacto = ""
+        try{if (igualEncontrado == true) { println("El número ya existe y este es su contacto: ${listaContactos[posicionIgual]}")}
+        else {println("Introduce un contacto para registrarlo con el número: "); contacto = readLine().toString(); while (Introducido != true) {if (listaNumeros[i].isNullOrEmpty()) {listaNumeros[i] = numero; listaContactos[i] = contacto; Introducido = true} else {i++}} }}
+        catch (e: IndexOutOfBoundsException) {listaContactos.add(contacto); listaNumeros.add(numero)}
+    }
+
+    fun listaContactosOrdenados(): List<Pair<String,String>>{
+        val listaAmbos = mutableListOf<Pair<String,String>>()
+        listaContactos.forEach {listaAmbos.add(Pair(it,listaNumeros.elementAt(listaContactos.indexOf(it))))}
+        return listaAmbos.sortedBy { it.first }
+    }
+
+    fun textoSimilar(textoABuscar: String): List<Pair<String,String>>{
+        val listaAmbos = mutableListOf<Pair<String,String>>()
+        //Hago lo mismo que en listaContactosOrdenados pero con un paso más. Solo añado aquellos cuyos pairs contiene algo del textoABuscar
+        val listaSimilar = mutableListOf<Pair<String,String>>()
+        listaContactos.forEach {listaAmbos.add(Pair(it,listaNumeros.elementAt(listaContactos.indexOf(it))))}
+        for (i in 0 until listaAmbos.size) {if (listaAmbos[i].first.contains(textoABuscar) || listaAmbos[i].second.contains(textoABuscar)) {listaSimilar.add(listaAmbos[i])}}
+        return listaSimilar
     }
 
 }
@@ -30,39 +49,74 @@ class Agenda(contacto: String = "", numero: String = ""){
 
 fun main() {
 
-    //Introduzco 10 nombres y 10 números para el funcionamiento del programa
-
+    //Utilizare esta variable para acceder a Agenda en adelante
+    var Input = Agenda()
 
     var input: String = ""
     println("Introduce un nombre o número: ")
     input = readLine().toString()
 
-    //Para comprobar si es un número compruebo con un while si contiene alguna letra, si es así esNombre saldrá true y lo meteré en el método de Contacto sino lo meteré en el de número
-    var esNombre = false; var i = 0; var Input = Agenda()
-    while (esNombre != true || i == input.length && esNombre == false) {if (input[i].isLetter()) {esNombre = true} else {i++}}
+
+    //Primero elimino los espacios dejados en el input (si hay) para que no haya problemas al compararlos
+    input = input.filter { !it.isWhitespace() }
+
+    //En las instrucciones ponia que la primera posición de una palabra tenia que ser una letra, así que si no es una letra será un número.
+    var esNombre = false
+    if (input[0].isLetter()) {esNombre = true} else esNombre = false
     if (esNombre == true) {Input.añadirContacto(input)} else {Input.añadirNumero(input)}
 
 
-    //Comandos especiales con switch
+    //No he terminado de entender como meter los comandos especiales. Como viene después de la explicación de tener que introducir un número o nombre en el programa
+    //voy a dar por hecho que es un switch después de terminar el algoritmo de haber introducido el número/nombre
+    //Tampoco termino de entender lo de filtrar texto a buscar. Texto a buscar de nombre? numero? Voy a dar por hecho que son ambos
+
+    println("----------------------------------------------------------")
+    println("Introduce un número referente a los comandos especiales:  ")
+    println("----------------------------------------------------------")
+    println("         1 - Adios (Terminar el programa)                 ")
+    println("                    2 - Listado                           ")
+    println("             3 - Filtrar Texto a buscar                   ")
+    println("----------------------------------------------------------")
+    var eleccion = readLine()?.toInt()
+
+    when(eleccion){
+
+        1 -> {println("Se acabó el programa.")}
+        2 -> {println(Input.listaContactosOrdenados())
+            println("----------------------------------------------------------")
+            println("Introduce un número referente a los comandos especiales:  ")
+            println("----------------------------------------------------------")
+            println("         1 - Adios (Terminar el programa)                 ")
+            println("                    2 - Listado                           ")
+            println("             3 - Filtrar Texto a buscar                   ")
+            println("----------------------------------------------------------")
+            eleccion = readLine()?.toInt()
+        }
+
+        3->{
+            println("Introduce el texto a buscar")
+            var textoABuscar: String = readLine().toString()
+            if (Input.textoSimilar(textoABuscar).isEmpty()) println("No hay ningún contacto") else println(Input.textoSimilar(textoABuscar))
+            println("----------------------------------------------------------")
+            println("Introduce un número referente a los comandos especiales:  ")
+            println("----------------------------------------------------------")
+            println("         1 - Adios (Terminar el programa)                 ")
+            println("                    2 - Listado                           ")
+            println("             3 - Filtrar Texto a buscar                   ")
+            println("----------------------------------------------------------")
+            eleccion = readLine()?.toInt()
+        }
+
+        else -> {println("No has introducido una opción válida, por favor introduce uno de los siguientes números:")
+            println("----------------------------------------------------------")
+            println("Introduce un número referente a los comandos especiales:  ")
+            println("----------------------------------------------------------")
+            println("         1 - Adios (Terminar el programa)                 ")
+            println("                    2 - Listado                           ")
+            println("             3 - Filtrar Texto a buscar                   ")
+            println("----------------------------------------------------------")
+            eleccion = readLine()?.toInt()
+        }
+    }
 
 }
-
-/*
-Crear un programa para gestionar una agenda de contactos. El funcionamiento será el siguiente:
-
-- El programa pedirá introducir algo por teclado:
-o Si se introduce un número de teléfono comprobará si existe, si es así mostrará a quién pertenece. Si no existe pedirá el nombre del contacto para darlo de alta.
-
-o Si se introduce un contacto debe comprobar si existe, si es así mostrará el número de teléfono. Si no existe pedirá el número de teléfono para darlo de alta.
-
-- Debe reconocer números de teléfono con espacios entre los dígitos o no y con el símbolo + al principio para indicar prefijo de país.
-- Los nombres deben comenzar por letras y pueden contener números y caracteres especiales imprimibles.
-
-- Comandos especiales:
-o adios: Sale del programa
-o listado: Muestra el listado completo de contactos ordenados por nombre
-o filtra texto_a_buscar: Muestra el listado de los contactos que contengan texto_a_buscar o “Ningún
-contacto” si no hubiera ninguno.
-
-- Se deben crear funciones cuando sea adecuado (por ejemplo, comprobar si existe un contacto en la agenda, comprobar si un texto puede ser un número de teléfono, etc).
- */
